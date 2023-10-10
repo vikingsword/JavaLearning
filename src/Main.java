515972f20788aa09e43169eb2305a46fa3deb38d
vikingar <423141759@qq.com>
2023-10-10 23:56:47 +0800

    thinking in java

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import thinkingInJava.Demo;
+
 /**
  * @author vikingar
  * @time 2023/10/10 23:17
@@ -5,5 +7,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Demo demo = new Demo();
+        demo.getInfo();
     }
 }
---
+++ src/thinkingInJava/Demo.java
@@ -0,0 +1,11 @@
+package thinkingInJava;
+
+/**
+ * @author vikingar
+ * @time 2023/10/10 23:53
+ */
+public class Demo {
+    public void getInfo() {
+        System.out.println("this is test");
+    }
+}
