22f79b43d5bbfc1e83d113abd156adda7426e7eb
vikingar <423141759@qq.com>
2023-10-10 23:24:03 +0800

    init

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 /**
- * @time 2023/10/10 23:17
  * @author vikingar
- */public class Main {
+ * @time 2023/10/10 23:17
+ */
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
