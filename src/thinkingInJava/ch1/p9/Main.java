0d04b399cdbba54a8d98fc5aac24cecc9d81680f
vikingar <423141759@qq.com>
2023-10-12 00:03:18 +0800

    thinking in java

---
+++ src/thinkingInJava/ch1/p9/Main.java
@@ -0,0 +1,12 @@
+package thinkingInJava.ch1.p9;
+
+/**
+ * @author: vikingar
+ * @time: 2023/10/12 0:03
+ * @description:
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
