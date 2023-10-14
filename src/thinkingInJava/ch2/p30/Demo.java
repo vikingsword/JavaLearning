ef2a34ca185c6a2894e521e7cd9cb22fa5006dbf
vikingar <423141759@qq.com>
2023-10-14 21:49:34 +0800

    thinking in java

---
+++ src/thinkingInJava/ch2/p30/Demo.java
@@ -0,0 +1,12 @@
+package thinkingInJava.ch2.p30;
+
+/**
+ * @author: vikingar
+ * @time: 2023/10/14 21:48
+ * @description:
+ */
+public class Demo {
+    public void test() {
+        System.out.println("this is branch test");
+    }
+}
