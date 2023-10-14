6bc182c0ab1cb65630dfa8a27f0bb33698dc78ea
vikingar <423141759@qq.com>
2023-10-14 22:48:33 +0800

    thinking in java

--- src/thinkingInJava/ch2/p30/BranchTest.java
+++ src/thinkingInJava/ch2/p30/BranchTest.java
@@ -5,7 +5,7 @@ package thinkingInJava.ch2.p30;
  * @time: 2023/10/14 21:48
  * @description:
  */
-public class Demo {
+public class BranchTest {
     public void test() {
         System.out.println("this is branch test");
         System.out.println("this is local pull request ");
