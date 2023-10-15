e759708a5f0c42b0c814b2a5e1133bc281dde0a8
vikingar <423141759@qq.com>
2023-10-15 22:23:32 +0800

    thinking in java

---
+++ src/thinkingInJava/ch2/p37/prac1/Ch2Practice1.java
@@ -0,0 +1,16 @@
+package thinkingInJava.ch2.p37.prac1;
+
+/**
+ * @author: vikingar
+ * @time: 2023/10/15 22:22
+ * @description:
+ */
+public class Ch2Practice1 {
+    public int a ;
+    public char c;
+
+    public void printInfo() {
+        System.out.println(a);
+        System.out.println(c);
+    }
+}
---
+++ src/thinkingInJava/ch2/p37/prac1/Main.java
@@ -0,0 +1,13 @@
+package thinkingInJava.ch2.p37.prac1;
+
+/**
+ * @author: vikingar
+ * @time: 2023/10/15 22:21
+ * @description:
+ */
+public class Main {
+    public static void main(String[] args) {
+        Ch2Practice1 demo = new Ch2Practice1();
+        demo.printInfo();
+    }
+}
