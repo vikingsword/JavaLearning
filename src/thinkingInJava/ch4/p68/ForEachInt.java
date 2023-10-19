dc625962084a1e9a07c246b4b2cc7f1b5e16975b
vikingar <423141759@qq.com>
2023-10-19 23:52:46 +0800

    thinking in java

--- src/thinkingInJava/ch4/p68/ForEachInt.java
+++ src/thinkingInJava/ch4/p68/ForEachInt.java
@@ -1,5 +1,10 @@
 package thinkingInJava.ch4.p68;
 
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 /**
  * @author: vikingar
  * @time: 2023/10/19 23:46
@@ -11,5 +16,14 @@ public class ForEachInt {
         for (int i : arr) {
             System.out.print(i + ", ");
         }
+
+        System.out.println("--------------");
+        List<Integer> list = new ArrayList<>();
+        Random random = new Random();
+        for (int i = 0; i < 10; i++) {
+            int num = random.nextInt(10);
+            list.add(num);
+        }
+        list.forEach(o -> {System.out.print(o + ", ");});
     }
 }
