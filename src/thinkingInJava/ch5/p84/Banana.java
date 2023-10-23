9d0434e6da1efd8a5a5db4b21466401e08ba8cd1
vikingar <423141759@qq.com>
2023-10-23 23:35:20 +0800

    thinking in java

---
+++ src/thinkingInJava/ch5/p84/Banana.java
@@ -0,0 +1,12 @@
+package thinkingInJava.ch5.p84;
+
+/**
+ * @author: vikingar
+ * @time: 2023/10/23 23:33
+ * @description:
+ */
+public class Banana {
+    void peel(int i) {
+        System.out.println("this is peel method");
+    }
+}
---
+++ src/thinkingInJava/ch5/p84/BananaPeel.java
@@ -0,0 +1,17 @@
+package thinkingInJava.ch5.p84;
+
+/**
+ * @author: vikingar
+ * @time: 2023/10/23 23:33
+ * @description:
+ */
+public class BananaPeel {
+
+    public static void main(String[] args) {
+        Banana a = new Banana(),
+        b = new Banana();
+        a.peel(1);
+        b.peel(2);
+    }
+}
+
