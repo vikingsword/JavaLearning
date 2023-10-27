b7ae307b10f3e96751ceaad50f375e8e62345a95
vikingar <423141759@qq.com>
2023-10-27 23:29:10 +0800

    thinking in java

---
+++ src/thinkingInJava/ch5/p85/p92/InitTest.java
@@ -0,0 +1,15 @@
+package thinkingInJava.ch5.p85.p92;
+
+/**
+ * @author: vikingar
+ * @time: 2023/10/26 23:35
+ * @description:
+ */
+public class InitTest {
+
+
+    public static void main(String[] args) {
+        int i;
+//        i++;
+    }
+}
---
+++ src/thinkingInJava/ch5/p85/p92/InitialValues.java
@@ -0,0 +1,16 @@
+package thinkingInJava.ch5.p85.p92;
+
+/**
+ * @author: vikingar
+ * @time: 2023/10/26 23:37
+ * @description:
+ */
+public class InitialValues {
+    int i;
+    boolean b;
+    char c;
+    float f;
+    double d;
+
+
+}
