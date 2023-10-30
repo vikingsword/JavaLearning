79cd07236cfc1ea3000ff9cacd79a94b8b3d9c45
vikingar <423141759@qq.com>
2023-10-30 18:49:55 +0800

    thinking in java

--- src/thinkingInJava/ch5/p93/Measurement.java
+++ src/thinkingInJava/ch5/p93/Measurement.java
@@ -8,8 +8,37 @@ package thinkingInJava.ch5.p93;
 public class Measurement {
     public static void main(String[] args) {
         Depth depth = new Depth();
-        System.out.println("depth = " + depth);
-
+        System.out.println("depth = " + depth.toString());
+        Depth2 depth2 = new Depth2();
+        depth2.print();
+        Depth2 depth21 = new Depth2();
+        depth21.setI(1);
+        int j = depth21.print2();
+        System.out.println(j);
 
     }
+}
+
+
+
+class Depth2 {
+
+    private int i;
+
+    Integer print2() {
+        return i;
+    }
+
+    public int getI() {
+        return i;
+    }
+
+    public void setI(int i) {
+        this.i = i;
+    }
+
+    void print() {
+        System.out.println("depth2");
+    }
+
 }
