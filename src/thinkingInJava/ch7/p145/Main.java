6437e8361f0ee3d959195c376174cb6336227559
vikingar <423141759@qq.com>
2023-11-06 23:07:05 +0800

    thinking in java

---
+++ src/thinkingInJava/ch7/p145/Main.java
@@ -0,0 +1,10 @@
+package thinkingInJava.ch7.p145;
+
+/**
+ * @author: vikingar
+ * @time: 2023/11/6 23:06
+ * @description:
+ */
+//public class Main extends TestFinal{
+//
+//}
---
+++ src/thinkingInJava/ch7/p145/TestFinal.java
@@ -0,0 +1,13 @@
+package thinkingInJava.ch7.p145;
+
+/**
+ * @author: vikingar
+ * @time: 2023/11/6 23:06
+ * @description:
+ */
+public final class TestFinal {
+
+    public void test() {
+        System.out.println("test");
+    }
+}
